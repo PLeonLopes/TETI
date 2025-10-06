4c148b7d4c4c598b3cbeb46c7917aa30bb0b9962
Vinícius Vieira <viniciuslma.unipecc@gmail.com>
2025-10-06 00:37:44 -0300

    feat: init projeto

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("API em execução!");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
